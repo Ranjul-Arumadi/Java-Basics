fcfe48e84d3237d3e5ad7e5b3373b74a9996a12d
Ranjul Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2022-09-24 21:01:40 +0530

    Create StringLengthWithoutBuiltInFunction2.java

---
+++ StringLengthWithoutBuiltInFunction2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class test{
+    public static void main(String [] args){
+        String a = "ranjul";
+		
+		
+		char ans[] = a.toCharArray();
+
+		int c=0;
+      
+		for(char x: ans){
+			c++;
+		}
+
+		System.out.println(c);
+    }
+}
